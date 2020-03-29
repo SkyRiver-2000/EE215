76c2a92d40bdec5581a41dc41895b68bfaad928d
SkyRiver-2000 <58808235+SkyRiver-2000@users.noreply.github.com>
2020-03-29 13:41:34 +0800

    Main program for sudoku problem

---
+++ Sudoku/go.m
@@ -0,0 +1,18 @@
+init_data;
+tic;
+cell = 1;
+while (cell <= Order * Order && cell > 0)
+    oneround;
+end
+if cell == Order * Order + 1
+    fprintf('\nIt took %4.3f s.\n', toc);
+    fprintf('The answer is:\n');
+    print_result;
+    elseif cell == 0
+        fprintf('\nThe puzzle has no answer!\n');
+end
+plot([1 : cell_record_ptr - 1], cell_record(1 : cell_record_ptr - 1));
+xlabel('Times of Iterations');
+ylabel('Current Cell');
+title('Performance Analysis');
+box on; grid on;
