1623cb849f8d3b66f319e162a57cdae1db140f2f
SkyRiver-2000 <58808235+SkyRiver-2000@users.noreply.github.com>
2020-04-05 15:19:03 +0800

    Select new individuals according to their fitness

--- Calibration Problem/select.m
+++ Calibration Problem/select.m
@@ -3,11 +3,13 @@ function next_gen = select(p, groups)
 % Improved by adding protection for the best individual
     [num_ind, num_features] = size(groups);
     temp = rand(num_ind, 1); % Stochastic number for roulette
-next_gen = false(num_ind, num_features);
-% Preserve the best individual of last generation and copy it
+    next_gen = false(num_ind, num_features);
+    
+    % Preserve the best individual of last generation and copy it
     next_gen(1, :) = groups(1, :);
-next_gen(2, :) = groups(1, :);
-% Select individuals according to the rand matrix
+    next_gen(2, :) = groups(1, :);
+    
+    % Select individuals according to the rand matrix
     for k = 3 : num_ind
         for m = 1 : num_ind
             if temp(k) <= p(m)
