a86f17a5bf6a0ce95e892a401cee8461e1b45b08
SkyRiver-2000 <58808235+SkyRiver-2000@users.noreply.github.com>
2020-04-05 15:15:22 +0800

    Cross two individuals' chromosomes

--- Calibration Problem/match.m
+++ Calibration Problem/match.m
@@ -2,13 +2,14 @@ function [new_1, new_2] = match(ind_1, ind_2, fm, f)
 % Cross the chromosomes of different individuals
 % Improved by self-adaptive probability of cross according to individual fitness 
     f_avg = mean(f);
-f_max = max(f);
+    f_max = max(f);
+    
 % Self-adaptive probability of cross
     if fm < f_avg
         p_match = 1;
     else
         p_match = 0.6 * (f_max - fm) / (f_max - f_avg) + 0.1;
-end
+    end
 
 % If cross happens, swap a fragment stochastically
 % Else just keep the two individuals unchanged
